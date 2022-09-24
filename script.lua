8e87bc25b59fabccead3cb8bb9329c4032ea8187
Kramelidk <89310413+Kramelidk@users.noreply.github.com>
2022-09-24 14:49:39 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -17,6 +17,8 @@ local Char = Instance.new("TextButton")
 local UICorner_8 = Instance.new("UICorner")
 local inf = Instance.new("TextButton")
 local UICorner_9 = Instance.new("UICorner")
+local afk = Instance.new("TextButton")
+local UICorner_10 = Instance.new("UICorner")
 
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
@@ -221,3 +223,36 @@ inf.MouseButton1Down:Connect(function()
 end)
 
 UICorner_9.Parent = inf
+
+afk.Name = "afk"
+afk.Parent = Frame
+afk.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+afk.Position = UDim2.new(0.50605054376, 0, 0.61200002, 0)
+afk.Size = UDim2.new(0, 200, 0, 49)
+afk.Font = Enum.Font.Ubuntu
+afk.Text = "Anti-Afk"
+afk.TextColor3 = Color3.fromRGB(0, 0, 0)
+afk.TextSize = 25.000
+afk.TextWrapped = true
+afk.MouseButton1Down:Connect(function()
+    wait(0.5)local ba=Instance.new("ScreenGui")
+    local ca=Instance.new("TextLabel")local da=Instance.new("Frame")
+    local _b=Instance.new("TextLabel")local ab=Instance.new("TextLabel")ba.Parent=game.CoreGui
+    ba.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;ca.Parent=ba;ca.Active=true
+    ca.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ca.Draggable=true
+    ca.Position=UDim2.new(0.698610067,0,0.098096624,0)ca.Size=UDim2.new(0,304,0,52)
+    ca.Font=Enum.Font.SourceSansSemibold;ca.Text="Anti Afk Kick Script"ca.TextColor3=Color3.new(0,1,1)
+    ca.TextSize=22;da.Parent=ca
+    da.BackgroundColor3=Color3.new(0.196078,0.196078,0.196078)da.Position=UDim2.new(0,0,1.0192306,0)
+    da.Size=UDim2.new(0,304,0,107)_b.Parent=da
+    _b.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)_b.Position=UDim2.new(0,0,0.800455689,0)
+    _b.Size=UDim2.new(0,304,0,21)_b.Font=Enum.Font.Arial;_b.Text="Moddified by Kramel"
+    _b.TextColor3=Color3.new(1,1,1)_b.TextSize=20;ab.Parent=da
+    ab.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ab.Position=UDim2.new(0,0,0.158377379,0)
+    ab.Size=UDim2.new(0,304,0,44)ab.Font=Enum.Font.ArialBold;ab.Text="Status: Script Has Started"
+    ab.TextColor3=Color3.new(1,1,1)ab.TextSize=20;local bb=game:service'VirtualUser'
+    game:service'Players'.LocalPlayer.Idled:connect(function()
+    bb:CaptureController()bb:ClickButton2(Vector2.new())
+    ab.Text="Kick Reflected! Re-Enabling Script..."wait(2)ab.Text="Script Re-Enabled"end)
+
+UICorner_10.Parent = afk
